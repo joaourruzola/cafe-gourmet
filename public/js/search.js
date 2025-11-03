b5460d647c368a3a6810642d1903c69e22f150bc
João Urruzola <joaoalbertobraun@gmail.com>
2025-11-02 21:53:25 -0300

    adiciona funcionalidade de scrollIntoView quando usuário pesquisa na barra de pesquisa

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const searchInput = document.getElementById("searchInput");
 	const productCards = document.querySelectorAll(".product-card");
+	const productList = document.getElementById("productList");
+	const navbar = document.querySelector(".navbar");
+
+	searchInput.addEventListener("focus", () => {
+		if (!productList) return;
+
+		let navbarHeight = 0;
+		if (navbar) {
+			navbarHeight = navbar.offsetHeight;
+		}
+
+		const productListTopPosition =
+			productList.getBoundingClientRect().top + window.scrollY;
+		const targetScrollPosition = productListTopPosition - navbarHeight - 10;
+
+		window.scrollTo({
+			top: targetScrollPosition,
+			behavior: "smooth",
+		});
+	});
 
 	searchInput.addEventListener("input", (event) => {
 		const searchTerm = event.target.value.toLowerCase().trim();
