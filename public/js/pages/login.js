2c57c8380c14e1a33766b620243b65a94e0018a8
João Urruzola <joaoalbertobraun@gmail.com>
2025-11-08 15:20:29 -0300

    remove comentários

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,3 +1,4 @@
+// redeclarado função fetchJSON, provisório até corrigir importação
 const fetchJSON = async (url, options = {}) => {
 	const res = await fetch(url, options);
 	if (!res.ok) {
@@ -18,13 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	if (loginForm) {
 		loginForm.addEventListener("submit", async (event) => {
-			event.preventDefault(); // Impede o envio padrão do formulário (o que causa o redirecionamento imediato)
+			event.preventDefault();
 
 			const formData = new FormData(loginForm);
 			const data = Object.fromEntries(formData.entries());
 
 			try {
-				// fetchJSON está definida em cart.js ou outro script global
 				const response = await fetchJSON("/auth/login", {
 					method: "POST",
 					headers: {
@@ -33,9 +33,8 @@ document.addEventListener("DOMContentLoaded", () => {
 					body: JSON.stringify(data),
 				});
 
-				// Sucesso (recebemos o JSON com o redirect)
+				// Sucesso
 				if (response.status === "sucesso" && response.redirect) {
-					// 1. Opcional: Mostrar Toast de sucesso antes de redirecionar
 					if (typeof showToast === "function") {
 						showToast(
 							response.mensagem || "Login bem-sucedido!",
@@ -44,12 +43,10 @@ document.addEventListener("DOMContentLoaded", () => {
 						);
 					}
 
-					// 2. Redirecionamento CLIENT-SIDE
 					setTimeout(() => {
 						window.location.href = response.redirect;
 					}, 200); // Pequeno delay para o toast aparecer
 				} else {
-					// Caso o backend retorne 200, mas sem redirect por algum erro inesperado
 					if (typeof showToast === "function") {
 						showToast(
 							"Resposta inesperada do servidor.",
@@ -59,18 +56,14 @@ document.addEventListener("DOMContentLoaded", () => {
 					}
 				}
 			} catch (error) {
-				// O fetchJSON está configurado para lançar um Error com a mensagem do JSON 401/400
 				const errorMessage =
 					error.message || "Falha na conexão ou erro desconhecido.";
 
-				// Exibe o alerta com a mensagem do JSON 401/400
 				if (typeof showToast === "function") {
 					showToast(errorMessage, "error", 5000);
 				} else {
 					alert(errorMessage);
 				}
-
-				// Nenhuma ação de redirecionamento aqui, o usuário permanece na tela de login.
 			}
 		});
 	}
